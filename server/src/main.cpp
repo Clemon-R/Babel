a46ff501ebff9964585500f5271d9bda26d87a51
Romain-P <romain.pillot@epitech.eu>
2018-10-07 23:14:28 +0200

    added server port option

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -12,10 +12,32 @@
 #include <boost/thread.hpp>
 
 int main(int ac, char *args[]) {
-    BabelController controller;
-    BabelServer server(controller, 4242);
-    controller.setServer(server);
+    boost::uint16_t port = NetworkServer::RANDOM_PORT;
 
-    server.run();
+    if (ac >= 2) {
+        port = (boost::uint16_t) atoi(args[1]);
+
+        if (port == 0) {
+            std::cerr << *args << ": invalid port" << std::endl;
+            std::cerr << "USAGE: " << *args << " [:port number]" << std::endl;
+            return 1;
+        }
+    } else
+        std::cout << "[babel-server] no port specified, random port will be used" << std::endl;
+
+    BabelController controller;
+    try {
+        BabelServer server(controller, port);
+        controller.setServer(server);
+
+        server.run(true);
+        std::this_thread::sleep_for(std::chrono::milliseconds(300));
+        std::cout << "[babel-server] listening on port " << server.getPort() << std::endl;
+        std::cout << "[babel-server] awaiting connections..." << std::endl;
+        server.getThread().join();
+    } catch (std::exception &e) {
+        std::cerr << *args << "an error has occurred (system port?): " << e.what() << std::endl;
+        return 1;
+    }
     return 0;
 }
